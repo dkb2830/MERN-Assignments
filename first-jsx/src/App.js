3c6eab77368aa863959158ccdfee121ffedd4bbb
dkb2830 <thebergbowers2017@gmail.com>
2021-10-31 21:49:04 -0400

    first jsx assignment

---
+++ first-jsx/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <><h1>Hello Dojo!</h1>
+    <h2>Things I need to do:</h2>
+    <ul>
+      <li class="str">Learn React</li>
+      <li class="str">Climb Mt. Everest</li>
+      <li class="str">Run a marathon</li>
+      <li class="str">Feed the dogs</li>
+    </ul>
+    </>
+    // <div className="App">
+      
+      /* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */
+    // </div>
+  );
+}
+
+export default App;
---
+++ how_long.js
@@ -0,0 +1,21 @@
+Number.prototype.isPrime = () => {
+    for (let i = 2; i < Math.sqrt(this); i++) {
+        if (this % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+const { performance } = require('perf_hooks');
+const start = performance.now();
+let primeCount = 0;
+let num = 2; // for math reasons, 1 is considered prime
+while (primeCount < 1e4) {
+    if (num.isPrime()) {
+        primeCount++;
+    }
+    num++;
+}
+console.log(`The 10,000th prime number is ${num}`);
+console.log(`This took ${performance.now() - start} milliseconds to run`);
