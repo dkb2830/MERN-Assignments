e78fe8decb1fd122a006fc62f1295e88bcf394fa
dkb2830 <thebergbowers2017@gmail.com>
2021-11-04 12:14:23 -0400

    add age increase button

--- Javascript/person-card/src/App.js
+++ Javascript/person-card/src/App.js
@@ -4,10 +4,10 @@ import Subcontents from './components/Subcontents';
 function App() {
   return (
     <div className="App">
-      <Subcontents firstName={'Jane'} lastName={'Doe'} age={'45'} hairColor={'Black'}/>
-      <Subcontents firstName={'John'} lastName={'Smith'} age={'88'} hairColor={'Brown'}/>
-      <Subcontents firstName={'Millard'} lastName={'Fillmore'} age={'50'} hairColor={'Brown'}/>
-      <Subcontents firstName={'Maria'} lastName={'Smith'} age={'62'} hairColor={'Brown'}/>
+      <Subcontents firstName={'Jane'} lastName={'Doe'} age={45} hairColor={'Black'}/>
+      <Subcontents firstName={'John'} lastName={'Smith'} age={88} hairColor={'Brown'}/>
+      <Subcontents firstName={'Millard'} lastName={'Fillmore'} age={50} hairColor={'Brown'}/>
+      <Subcontents firstName={'Maria'} lastName={'Smith'} age={62} hairColor={'Brown'}/>
     </div>
   );
 }
--- Javascript/person-card/src/components/Subcontents.js
+++ Javascript/person-card/src/components/Subcontents.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Subcontents = (props) => {
-    const { firstName, lastName, age, hairColor } = props;
+    const { firstName, lastName, hairColor } = props;
+    const [ age, setAge ] = useState(props.age)
     return (
-            <div className='Subcontents'>
-                <h1>{lastName}, {firstName}</h1>
-                <p>Age: {age}</p>
-                <p>Hair Color: {hairColor}</p>
-            </div>
+        <div className='Subcontents'>
+            <h1>{lastName}, {firstName}</h1>
+            <p>Age: {age}</p>
+            <p>Hair Color: {hairColor}</p>
+            <button onClick={(event) => setAge(age + 1)}>Birthday Button for {firstName} {lastName}</button>
+        </div>
     );
 };
 
