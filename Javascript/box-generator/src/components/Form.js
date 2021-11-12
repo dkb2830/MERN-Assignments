fa1d7809e46d1e4a49b55278712686670ca1e891
dkb2830 <thebergbowers2017@gmail.com>
2021-11-12 12:34:15 -0500

    add color

--- Javascript/box-generator/src/components/Form.js
+++ Javascript/box-generator/src/components/Form.js
@@ -9,27 +9,29 @@ const Form = (props) => {
         const newBox = {
             color: color
         };
-
-        const box = {
-            height: 50px;
-            width: 50px;
-            color: {color};
-        }
-
     };
+
+    const box = {
+        height: "50px",
+        width: "50px",
+        border: "5px solid black",
+        backgroundColor: "color",
+    }
+
+
 
     return (
         <>
         <form onSubmit = {createBox}>
             <div>
                 <label>Color:</label>
-                <input type="text"  value = "color" onChange= {(e) => setColor(e.target.value) }/>
+                <input type="text"  onChange= {(e) => setColor(e.target.value) }/>
             </div>
             <div>
                 <input type="submit" value="Add"/>
             </div>
         </form>
-        <div style= "box">
+        <div style= {box}>
 
         </div>
         </>
