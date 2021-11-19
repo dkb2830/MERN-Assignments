ac6dd857a3d8a96d274b319a8deb766ec36fbf5d
dkb2830 <thebergbowers2017@gmail.com>
2021-11-18 22:41:31 -0500

    fixed url

--- Javascript/pokemon-api/src/components/Example.js
+++ Javascript/pokemon-api/src/components/Example.js
@@ -1,26 +1,27 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const bullet = {
     listStyleType: "circle",
     textTransform: "capitalize",
 }
 const Example = (props) => {
-    const [state, setState] = useState(0);
+    const [state, setState] = useState([]);
     useEffect(() => {
         console.log("Hello")
-        fetch('https://pokeapi.co/api/v2/pokemon/')
+        axios.get('https://pokeapi.co/api/v2/pokemon?limit=807')
+            // .then(response => {
+            //     return response.json()
+            // })
             .then(response => {
-                return response.json()
-            })
-            .then(response => {
-                setState({
-                    people: response.results
-                })
+                setState(
+                    response.data.results
+                )
             })
     }, []);
     return (
         <div>
-            {state.people ? state.people.map((item, index) => {
+            {state ? state.map((item, index) => {
                 return (<div key={index}><li className={bullet}>{item.name}</li></div>)
             }) : null}
         </div>
